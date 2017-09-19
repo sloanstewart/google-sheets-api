0e1ffa14c263fdf954648d23242de33ee4593835
Sloan <sloanstewartdev@gmail.com>
2017-09-19 16:01:21 -0400

    TODO: Add pivot table and chart

--- start/routes.js
+++ start/routes.js
@@ -24,18 +24,19 @@ var router = express.Router();
 var models = require('./models');
 var Sequelize = require('sequelize');
 
-// TODO: Show spreadsheets on the main page.
+// Show spreadsheets on the main page.
 router.get('/', function(req, res, next) {
   var options = {
     order: [['createdAt', 'DESC']]
   };
-  models.Order.findAll(options)
-  .then(function(orders) {
+  Sequelize.Promise.all([
+    models.Order.findAll(options),
+    models.Spreadsheet.findAll(options)
+  ]).then(function(results) {
     res.render('index', {
-      orders: orders
+      orders: results[0],
+      spreadsheets: results[1]
     });
-  }, function(err) {
-    next(err);
   });
 });
 
@@ -78,12 +79,55 @@ router.post('/upsert', function(req, res, next) {
   });
 });
 
-// TODO: Add route for creating spreadsheet.
+// route for creating spreadsheet.
+var SheetsHelper = require('./sheets');
+
+router.post('/spreadsheets', function(req, res, next) {
+  var auth = req.get('Authorization');
+  if (!auth) {
+    return next(Error('Authorization required.'));
+  }
+  var accessToken = auth.split(' ')[1];
+  var helper = new SheetsHelper(accessToken);
+  var title = 'Orders (' + new Date().toLocaleTimeString() + ')';
+  helper.createSpreadsheet(title, function(err, spreadsheet) {
+    if (err) {
+      return next(err);
+    }
+    var model = {
+      id: spreadsheet.spreadsheetId,
+      sheetId: spreadsheet.sheets[0].properties.sheetId,
+      name: spreadsheet.properties.title
+    };
+    models.Spreadsheet.create(model).then(function() {
+      return res.json(model);
+    });
+  });
+});
 
 
-
-// TODO: Add route for syncing spreadsheet.
-
+// route for syncing spreadsheet.
+router.post('/spreadsheets/:id/sync', function(req, res, next) {
+  var auth = req.get('Authorization');
+  if (!auth) {
+    return next(Error('Authorization required.'));
+  }
+  var accessToken = auth.split(' ')[1];
+  var helper = new SheetsHelper(accessToken);
+  Sequelize.Promise.all([
+    models.Spreadsheet.findById(req.params.id),
+    models.Order.findAll()
+  ]).then(function(results) {
+    var spreadsheet = results[0];
+    var orders = results[1];
+    helper.sync(spreadsheet.id, spreadsheet.sheetId, orders, function(err) {
+      if (err) {
+        return next(err);
+      }
+      return res.json(orders.length);
+    });
+  });
+});
 
 
 module.exports = router;
---
+++ start/sheets.js
@@ -0,0 +1,207 @@
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var util = require('util');
+
+var SheetsHelper = function(accessToken) {
+  var authClient = new googleAuth();
+  var auth = new authClient.OAuth2();
+  auth.credentials = {
+    access_token: accessToken
+  };
+  this.service = google.sheets({version: 'v4', auth: auth});
+};
+
+module.exports = SheetsHelper;
+
+SheetsHelper.prototype.createSpreadsheet = function(title, callback) {
+  var self = this;
+  var request = {
+    resource: {
+      properties: {
+        title: title
+      },
+      sheets: [
+        {
+          properties: {
+            title: 'Data',
+            gridProperties: {
+              columnCount: 6,
+              frozenRowCount: 1
+            }
+          }
+        },
+        // TODO: Add more sheets.
+      ]
+    }
+  };
+  self.service.spreadsheets.create(request, function(err, spreadsheet) {
+    if (err) {
+      return callback(err);
+    }
+    // TODO: Add header rows.
+
+    var dataSheetId = spreadsheet.sheets[0].properties.sheetId;
+    var requests = [
+      buildHeaderRowRequest(dataSheetId),
+    ];
+    // TODO: Add pivot table and chart.
+
+
+    var request = {
+      spreadsheetId: spreadsheet.spreadsheetId,
+      resource: {
+        requests: requests
+      }
+    };
+    self.service.spreadsheets.batchUpdate(request, function(err, response) {
+      if (err) {
+        return callback(err);
+      }
+      return callback(null, spreadsheet);
+    });
+  });
+};
+
+var COLUMNS = [
+  { field: 'id', header: 'ID' },
+  { field: 'customerName', header: 'Customer Name'},
+  { field: 'productCode', header: 'Product Code' },
+  { field: 'unitsOrdered', header: 'Units Ordered' },
+  { field: 'unitPrice', header: 'Unit Price' },
+  { field: 'status', header: 'Status'}
+];
+
+function buildHeaderRowRequest(sheetId) {
+  var cells = COLUMNS.map(function(column) {
+    return {
+      userEnteredValue: {
+        stringValue: column.header
+      },
+      userEnteredFormat: {
+        textFormat: {
+          bold: true
+        }
+      }
+    }
+  });
+  return {
+    updateCells: {
+      start: {
+        sheetId: sheetId,
+        rowIndex: 0,
+        columnIndex: 0
+      },
+      rows: [
+        {
+          values: cells
+        }
+      ],
+      fields: 'userEnteredValue,userEnteredFormat.textFormat.bold'
+    }
+  };
+}
+
+SheetsHelper.prototype.sync = function(spreadsheetId, sheetId, orders, callback) {
+  var requests = [];
+  // Resize the sheet.
+  requests.push({
+    updateSheetProperties: {
+      properties: {
+        sheetId: sheetId,
+        gridProperties: {
+          rowCount: orders.length + 1,
+          columnCount: COLUMNS.length
+        }
+      },
+      fields: 'gridProperties(rowCount,columnCount)'
+    }
+  });
+  // Set the cell values.
+  requests.push({
+    updateCells: {
+      start: {
+        sheetId: sheetId,
+        rowIndex: 1,
+        columnIndex: 0
+      },
+      rows: buildRowsForOrders(orders),
+      fields: '*'
+    }
+  });
+  // Send the batchUpdate request.
+  var request = {
+    spreadsheetId: spreadsheetId,
+    resource: {
+      requests: requests
+    }
+  };
+  this.service.spreadsheets.batchUpdate(request, function(err) {
+    if (err) {
+      return callback(err);
+    }
+    return callback();
+  });
+};
+
+function buildRowsForOrders(orders) {
+  return orders.map(function(order) {
+    var cells = COLUMNS.map(function(column) {
+      switch (column.field) {
+        case 'unitsOrdered':
+          return {
+            userEnteredValue: {
+              numberValue: order.unitsOrdered
+            },
+            userEnteredFormat: {
+              numberFormat: {
+                type: 'NUMBER',
+                pattern: '#,##0'
+              }
+            }
+          };
+          break;
+        case 'unitPrice':
+          return {
+            userEnteredValue: {
+              numberValue: order.unitPrice
+            },
+            userEnteredFormat: {
+              numberFormat: {
+                type: 'CURRENCY',
+                pattern: '"$"#,##0.00'
+              }
+            }
+          };
+          break;
+        case 'status':
+          return {
+            userEnteredValue: {
+              stringValue: order.status
+            },
+            dataValidation: {
+              condition: {
+                type: 'ONE_OF_LIST',
+                values: [
+                  { userEnteredValue: 'PENDING' },
+                  { userEnteredValue: 'SHIPPED' },
+                  { userEnteredValue: 'DELIVERED' }
+                ]
+              },
+              strict: true,
+              showCustomUi: true
+            }
+          };
+          break;
+        default:
+          return {
+            userEnteredValue: {
+              stringValue: order[column.field].toString()
+            }
+          };
+      }
+    });
+    return {
+      values: cells
+    };
+  });
+}
