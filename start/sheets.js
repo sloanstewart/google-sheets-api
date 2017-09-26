1dbf2d31cd43c8ee854f48f647b03d5ae3c16695
Sloan <sloanstewartdev@gmail.com>
2017-09-26 14:02:59 -0400

    added pivot table & chart

--- start/sheets.js
+++ start/sheets.js
@@ -30,7 +30,15 @@ SheetsHelper.prototype.createSpreadsheet = function(title, callback) {
             }
           }
         },
-        // TODO: Add more sheets.
+        // Add more sheets.
+        {
+          properties: {
+            title: 'Pivot',
+            gridProperties: {
+              hideGridlines: true
+            }
+          }
+        }
       ]
     }
   };
@@ -38,14 +46,18 @@ SheetsHelper.prototype.createSpreadsheet = function(title, callback) {
     if (err) {
       return callback(err);
     }
-    // TODO: Add header rows.
-
+    // Add header rows.
     var dataSheetId = spreadsheet.sheets[0].properties.sheetId;
     var requests = [
       buildHeaderRowRequest(dataSheetId),
     ];
-    // TODO: Add pivot table and chart.
-
+    // Add pivot table and chart.
+    var pivotSheetId = spreadsheet.sheets[1].properties.sheetId;
+    requests = requests.concat([
+      buildPivotTableRequest(dataSheetId, pivotSheetId),
+      buildFormatPivotTableRequest(pivotSheetId),
+      buildAddChartRequest(pivotSheetId)
+    ]);
 
     var request = {
       spreadsheetId: spreadsheet.spreadsheetId,
@@ -82,7 +94,7 @@ function buildHeaderRowRequest(sheetId) {
           bold: true
         }
       }
-    }
+    };
   });
   return {
     updateCells: {
@@ -203,5 +215,119 @@ function buildRowsForOrders(orders) {
     return {
       values: cells
     };
+  });
+}
+
+function buildPivotTableRequest(sourceSheetId, targetSheetId) {
+  return {
+    updateCells: {
+      start: { sheetId: targetSheetId, rowIndex: 0, columnIndex: 0 },
+      rows: [
+        {
+          values: [
+            {
+              pivotTable: {
+                source: {
+                  sheetId: sourceSheetId,
+                  startRowIndex: 0,
+                  startColumnIndex: 0,
+                  endColumnIndex: COLUMNS.length
+                },
+                rows: [
+                  {
+                    sourceColumnOffset: getColumnForField('productCode').index,
+                    showTotals: false,
+                    sortOrder: 'ASCENDING'
+                  }
+                ],
+                values: [
+                  {
+                    summarizeFunction: 'SUM',
+                    sourceColumnOffset: getColumnForField('unitsOrdered').index
+                  },
+                  {
+                    summarizeFunction: 'SUM',
+                    name: 'Revenue',
+                    formula: util.format("='%s' * '%s'",
+                        getColumnForField('unitsOrdered').header,
+                        getColumnForField('unitPrice').header)
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      ],
+      fields: '*'
+    }
+  };
+}
+
+function buildFormatPivotTableRequest(sheetId) {
+  return {
+    repeatCell: {
+      range: { sheetId: sheetId, startRowIndex: 1, startColumnIndex: 2 },
+      cell: {
+        userEnteredFormat: {
+          numberFormat: { type: 'CURRENCY', pattern: '"$"#,##0.00' }
+        }
+      },
+      fields: 'userEnteredFormat.numberFormat'
+    }
+  };
+}
+
+function buildAddChartRequest(sheetId) {
+  return {
+    addChart: {
+      chart: {
+        spec: {
+          title: 'Revenue per Product',
+          basicChart: {
+            chartType: 'BAR',
+            legendPosition: 'RIGHT_LEGEND',
+            domains: [
+              // Show a bar for each product code in the pivot table.
+              {
+                domain: { sourceRange: { sources: [{
+                  sheetId: sheetId,
+                  startRowIndex: 0,
+                  startColumnIndex: 0,
+                  endColumnIndex: 1
+                }]}}
+              }
+            ],
+            series: [
+              // Set that bar's length based on the total revenue.
+              {
+                series: { sourceRange: { sources: [{
+                  sheetId: sheetId,
+                  startRowIndex: 0,
+                  startColumnIndex: 2,
+                  endColumnIndex: 3
+                }]}}
+              }
+            ]
+          }
+        },
+        position: {
+          overlayPosition: {
+            anchorCell: { sheetId: sheetId, rowIndex: 0, columnIndex: 3 },
+            widthPixels: 600,
+            heightPixels: 400
+          }
+        }
+      }
+    }
+  };
+}
+
+function getColumnForField(field) {
+  return COLUMNS.reduce(function(result, column, i) {
+    if (column.field == field) {
+      column.index = i;
+      return column;
+    }
+    return result;
   });
 }
